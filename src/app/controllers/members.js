485c88fd36ec0ecb390a52afdcc67a659c9b56bc
Hillary Reis <hillary.reis2002@gmail.com>
2021-03-01 19:00:35 -0300

    Ajustando controller de membros com os mesmos conceitos do controller de instrutores

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,8 +1,11 @@
 const { age, date } = require ('../../lib/utils');
+const Member = require ('../models/member');
 
 module.exports = {
   index (req, res) {
-    return res.render ("members/index");
+    Member.all(function (members) {
+      return res.render ("members/index", { members });
+    });
   },
   create (req, res) {
     return res.render ('members/create');
@@ -16,13 +19,27 @@ module.exports = {
       };
     };
   
-    return 
+    Member.create(req.body, function (member) {
+      return res.redirect(`members/${member.id}`);
+    });
   },
   show (req, res) {
-    return
+    Member.find(req.params.id, function(member) {
+      if (!member) return res.send ("Member not found!");
+
+      member.birth = date(member.birth).birthDay;
+
+      return res.render ('members/show', { member });
+    });
   },
   edit (req, res) {
-    return
+    Member.find(req.params.id, function(member) {
+      if (!member) return res.send ("Member not found!");
+
+      member.birth = date(member.birth).iso;
+
+      return res.render ('members/edit', { member });
+    });
   },
   put (req, res) {
     const keys = Object.keys (req.body);
@@ -33,9 +50,13 @@ module.exports = {
       };
     };
 
-    return
+    Member.update(req.body, function () {
+      return res.redirect(`/members/${req.body.id}`);
+    });
   },
   delete (req, res) {
-    return
+    Member.delete(req.body.id, function () {
+      return res.redirect('/members');
+    });
   },
 }
