c958a2661bafccfc07561ea9498724960c3e9f29
Hillary Reis <hillary.reis2002@gmail.com>
2021-03-01 16:20:31 -0300

    criando models e aprofundando em callback function

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,12 +1,9 @@
-const { age, date } = require ('../../lib/utils');
-const db = require ('../../config/db');
+const Instructor = require ('../models/instructor');
 
 module.exports = {
   index (req, res) {
-    db.query (`SELECT * FROM instructors`, function (err, results) {
-      if(err) return res.send("Database Error!");
-      
-      return res.render ("instructors/index", {instructors: results.rows});
+    Instructor.all(function (instructors) {
+      return res.render ("instructors/index", { instructors });
     });
   },
   create (req, res) {
@@ -21,31 +18,8 @@ module.exports = {
       };
     };
   
-    const query = `
-      INSERT INTO instructors (
-        name,
-        avatar_url,
-        gender,
-        services,
-        birth,
-        created_at
-      ) VALUES ($1, $2, $3, $4, $5, $6)
-      RETURNING id
-    `
-
-    const values = [
-      req.body.name,
-      req.body.avatar_url,
-      req.body.gender,
-      req.body.services,
-      date(req.body.birth).iso,
-      date(Date.now()).iso,
-    ];
-
-    db.query(query, values, function (err, results) {
-      if(err) return res.send("Database Error!");
-      
-      return res.redirect(`instructors/${results.rows[0].id}`);
+    Instructor.create(req.body, function (instructor) {
+      return res.redirect(`instructors/${instructor.id}`);
     });
   },
   show (req, res) {
