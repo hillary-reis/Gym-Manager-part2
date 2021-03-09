fa80bf73b5fcdc156e4f1dd3dd6f7811aa487ec3
Hillary Reis <hillary.reis2002@gmail.com>
2021-03-09 14:58:38 -0300

    Relacionando tabelas de instrutores e alunos no BD

--- src/app/models/instructor.js
+++ src/app/models/instructor.js
@@ -4,9 +4,12 @@ const db = require ('../../config/db');
 module.exports = {
   all(callback) {
     db.query (`
-      SELECT *
-      FROM instructors
-      ORDER BY name ASC`,
+      SELECT instructors.*,
+      count(members) AS total_students
+      FROM instructors 
+      LEFT JOIN members ON (members.instructor_id = instructors.id)
+      GROUP BY instructors.id
+      ORDER BY total_students DESC`,
       function (err, results) {
         if(err) throw `Database Error! ${err}`;
 
