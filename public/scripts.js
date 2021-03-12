277e66fb2d3500447de92d20076183b0eb58c4b8
Hillary Reis <hillary.reis2002@gmail.com>
2021-03-12 18:30:49 -0300

    Ajustes finais

--- public/scripts.js
+++ public/scripts.js
@@ -40,27 +40,33 @@ function paginate (selectedPage, totalPages) {
   return pages;
 };
 
-const pagination = document.querySelector('.pagination');
+function createPagination () {
+  // o + na frente transforma em número
+  const filter = pagination.dataset.filter;
+  const page = +pagination.dataset.page;
+  const total = +pagination.dataset.total;
 
-// o + na frente transforma em número
-const filter = pagination.dataset.filter;
-const page = +pagination.dataset.page;
-const total = +pagination.dataset.total;
+  const pages = paginate(page, total);
 
-const pages = paginate(page, total);
+  let elements = '';
 
-let elements = '';
-
-for (let page of pages) {
-  if (String(page).includes("...")) {
-    elements += `<span>${page}</span>`;
-  } else {
-    if (filter) {
-      elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`;
+  for (let page of pages) {
+    if (String(page).includes("...")) {
+      elements += `<span>${page}</span>`;
     } else {
-      elements += `<a href="?page=${page}">${page}</a>`;
+      if (filter) {
+        elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`;
+      } else {
+        elements += `<a href="?page=${page}">${page}</a>`;
+      };
     };
   };
+
+  pagination.innerHTML = elements;
 };
 
-pagination.innerHTML = elements;
+const pagination = document.querySelector('.pagination');
+
+if (pagination) {
+  createPagination(pagination);
+};
